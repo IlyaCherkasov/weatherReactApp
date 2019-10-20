f00f5d6ae553e843a9ad687877218e51912cb109
Cherkasik <ilyakopeysk@gmail.com>
2019-10-20 18:40:48 +0300

    added geolocation request

--- src/App.js
+++ src/App.js
@@ -13,17 +13,36 @@ const Container = styled.div`
   }
 `;
 
-function App(props) {
-  const { loading } = props;
-  return (
-    <Container>
-      <Header />
-      {loading
-        ? <Preloader />
-        : <CurWeather />}
-      <Favourites />
-    </Container>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    let userLocation = {
+      latitude: 59.860656899999995,
+      longitude: 30.317027299999996,
+    };
+    this.state = { userLocation: userLocation };
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition((location) => {
+      this.setState({ userLocation: location.coords })
+    });
+  }
+
+  render() {
+
+    const {loading} = this.props;
+    console.log(this.state.userLocation);
+    return (
+      <Container>
+        <Header/>
+        {loading
+          ? <Preloader/>
+          : <CurWeather/>}
+        <Favourites/>
+      </Container>
+    );
+  }
 }
 
 export default App;
