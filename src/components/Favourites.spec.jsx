319c44aa57af3363dca85383967e4e69acd20a35
Ilya <ilyakopeysk@gmail.com>
2019-12-07 11:53:22 +0300

    working renderWith

--- src/components/Favourites.spec.jsx
+++ src/components/Favourites.spec.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import 'jest-styled-components';
-import { render, fireEvent } from '@testing-library/react';
-import { Provider } from 'react-redux';
+import { fireEvent } from '@testing-library/react';
+import { renderWithRedux as render } from "../helpers/renderWith";
 import Favourites from './Favourites';
-import { store } from '../helpers/store';
 
-// TODO: render with function from global state
 describe('Favourites component', () => {
-  it('should render correctly', () => {
-    const { container, getByPlaceholderText, getByTestId } = render(
-      <Provider store={store}>
-        <Favourites />
-      </Provider>
-    );
+  it('should render correctly without children', () => {
+    const { container, getByPlaceholderText, getByTestId } = render(<Favourites />);
     expect(container.firstChild).toMatchSnapshot();
     fireEvent.change(getByPlaceholderText('Добавить новый город'), { target: { value: 'Moscow' }});
     expect(getByPlaceholderText('Добавить новый город').value).toBe('Moscow');
     // submit - synthetic event that options should be written
     fireEvent.submit(getByTestId('form'), { target: [{ value: 'Moscow' }] });
-    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('should render correctly with children', () => {
+    const { container } = render(<Favourites />, { initialState: { favourites: { favourites: ['Moscow'] } } });
+    expect(container).toMatchSnapshot();
   });
 });
--- src/helpers/renderWith.js
+++ src/helpers/renderWith.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { createStore } from 'redux';
+import thunkMiddleware from "redux-thunk";
+import {applyMiddleware, compose, createStore} from 'redux';
 import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
 import reducer from '../store/rootReducer';
 
 export function renderWithRedux(
   ui,
-  { initialState, store = createStore(reducer, initialState) } = {}
+  { initialState, store = compose(applyMiddleware(thunkMiddleware))(createStore)(reducer, initialState) } = {}
 ) {
   return {
     ...render(<Provider store={store}>{ui}</Provider>),
